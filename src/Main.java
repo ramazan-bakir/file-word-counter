379d088cebbbed875b21545067fc7326f838a96f
Ozgun35 <48850196+Ozgun35@users.noreply.github.com>
2019-04-20 17:41:29 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,442 @@
-import java.util.Scanner;
-public class Main {
+import java.io.*;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.IOException;
+import java.io.BufferedReader;
+//import org.apache.poi.hwpf.HWPFDocument;
 
-  public static void main(String[] args)
-    {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input the string: ");
-        String str = in.nextLine();
-
-        System.out.print("Number of words in the string: " + count_Words(str)+"\n");
+/**
+ *
+ * @author Ramazan
+ */
+public class MainScreen extends javax.swing.JFrame {
+    
+    
+    
+    public MainScreen() {
+        initComponents();
+        this.setLocationRelativeTo(null);// Projenin merkezde durması için
     }
 
- public static int count_Words(String str)
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        PathField = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        SelectButton = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtsonuc = new javax.swing.JTextArea();
+        jLabel6 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        PathField.setEditable(false);
+        PathField.setBackground(new java.awt.Color(203, 236, 248));
+        PathField.setFont(new java.awt.Font("Tahoma", 2, 16)); // NOI18N
+        PathField.setForeground(new java.awt.Color(135, 131, 131));
+        PathField.setText("File Name");
+        PathField.setPreferredSize(new java.awt.Dimension(151, 35));
+        PathField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                PathFieldActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("Word");
+        jButton1.setMaximumSize(new java.awt.Dimension(77, 25));
+        jButton1.setMinimumSize(new java.awt.Dimension(77, 25));
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        SelectButton.setText("Select a File");
+        SelectButton.setPreferredSize(new java.awt.Dimension(151, 35));
+        SelectButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                SelectButtonActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Letter");
+        jButton2.setPreferredSize(new java.awt.Dimension(151, 35));
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        txtsonuc.setColumns(20);
+        txtsonuc.setRows(5);
+        jScrollPane1.setViewportView(txtsonuc);
+
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Background.jpg"))); // NOI18N
+        jLabel6.setText("jLabel6");
+
+        jPanel1.setBackground(new java.awt.Color(82, 179, 217));
+
+        jLabel1.setFont(new java.awt.Font("Bodoni MT Condensed", 0, 40)); // NOI18N
+        jLabel1.setText("File Word Counter Program");
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
+        jLabel2.setText("X");
+        jLabel2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jLabel2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel2MouseClicked(evt);
+            }
+        });
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 48)); // NOI18N
+        jLabel3.setText("-");
+        jLabel3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jLabel3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel3MouseClicked(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 237, Short.MAX_VALUE)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(48, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(24, 24, 24))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(250, 250, 250)
+                .addComponent(PathField, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(20, 20, 20)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 260, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(SelectButton, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 530, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 590, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(40, 40, 40)
+                .addComponent(PathField, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(80, 80, 80)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(160, 160, 160)
+                .addComponent(SelectButton, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(360, 360, 360)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(94, 94, 94)
+                .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, 490, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+   /* public void readDocFile(String fileName) throws FileNotFoundException{
+        try{
+            File file =new File(fileName);
+            FileInputStream fis =new FileInputStream(file.getAbsolutePath());
+            HWPFDocument doc = new HWPFDocument(fis);
+            WordExtractor we = new WordExtractor(doc);
+            String[] paragraphs = we.getParagraphText();
+			
+			System.out.println("Total no of paragraph "+paragraphs.length);
+			for (String para : paragraphs) {
+				System.out.println(para.toString());
+			}
+			fis.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		
+        }
+        
+        
+    }
+    */
+    
+    
+    /*public void _PdfReader_(String fileName)
     {
-       int count = 0;
-        if (!(" ".equals(str.substring(0, 1))) || !(" ".equals(str.substring(str.length() - 1))))
+            PDDocument document = PDDocument.load(new File(fileName));
+            if (!document.isEncrypted()) {
+                PDFTextStripper stripper = new PDFTextStripper();
+                String text = stripper.getText(document);
+                txtsonuc.setText(text);
+            }
+document.close();
+    }*/
+    
+    
+    public void TxtReader(String fileName)
+    {
+    String line = null;
+        try {
+            File fileDir = 
+                new File(fileName);
+            BufferedReader in = new BufferedReader(
+		   new InputStreamReader(
+                      new FileInputStream(fileDir), "ISO-8859-9"));
+               txtsonuc.setText("");
+            while((line = in.readLine()) != null) {
+               txtsonuc.append(line+"\n");
+      //-------------------------------------------------------------//
+////             String a=null;
+////                try{
+////                    File fileWord=
+////                            new File(fileName);
+////                    BufferedReader br=new BufferedReader(
+////                    
+////                            new InputStreamReader(
+////                                    new FileInputStream(fileWord),"ISO-8859-9"));               
+////                    txtsonuc.setText("");
+////                    while((a = br.readLine()) !=null ){
+////                        txtsonuc.append(br+"\n");
+////                    }
+////                
+////                }
+////                catch(IOException ex){
+////                    
+////                }
+////            } 
+//            BufferedReader br = new BufferedReader(in);
+//               String a = br.readLine ();
+//               int count = 0;
+//               while (a != null) {
+//               String []parts = a.split(" ");
+//               for( String w : parts)
+//               {
+//               count++;        
+//               }
+//                a = br.readLine();
+//                txtsonuc.setText("");
+               }
+            in.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'");                  
+           
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+   /* public  void readDocxFile(String fileName) {
+
+		try {
+			File file = new File(fileName);
+			FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+
+			XWPFDocument document = new XWPFDocument(fis);
+
+			List<XWPFParagraph> paragraphs = document.getParagraphs();
+			
+			System.out.println("Total no of paragraph "+paragraphs.size());
+			for (XWPFParagraph para : paragraphs) {
+				System.out.println(para.getText());
+			}
+			fis.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	} */
+
+    
+    private void SelectButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SelectButtonActionPerformed
+        
+        JFileChooser fc = new JFileChooser();
+       
+        FileNameExtensionFilter filter1 =new FileNameExtensionFilter(
+        "PDF","pdf");
+        FileNameExtensionFilter filter2 =new FileNameExtensionFilter(
+        "DOC","doc");
+        FileNameExtensionFilter filter3 =new FileNameExtensionFilter(
+        "DOCX","docx");
+        FileNameExtensionFilter filter4 =new FileNameExtensionFilter(
+        "TXT","txt");
+        fc.setFileFilter(filter1);
+        fc.setFileFilter(filter2);
+        fc.setFileFilter(filter3);
+        fc.setFileFilter(filter4);
+        
+        int returnVal;
+        String path ="";
+        String fileName = "";
+
+        fc.setCurrentDirectory(new java.io.File("C:\\Users\\Ramazan\\Desktop"));
+        fc.setDialogTitle("Select A File");
+        returnVal = fc.showDialog(this, "Select");
+        
+        if(returnVal == JFileChooser.APPROVE_OPTION) {
+            
+             path  =   fc.getSelectedFile().getName();
+             fileName= fc.getSelectedFile().getAbsolutePath();
+             PathField.setText(path);
+           
+        }
+        
+       
+       if(getFileExtension(path).equals("txt") || getFileExtension(path).equals("docx") || getFileExtension(path).equals("doc") || getFileExtension(path).equals("pdf"))       
+       {
+         TxtReader(fileName);
+       }
+       else
+         txtsonuc.setText("Geçirsiz Format");
+
+     
+    }//GEN-LAST:event_SelectButtonActionPerformed
+    
+    private String getFileExtension(String file)
+    {
+        String Extension;
+        int index = file.lastIndexOf('.');
+        if (index >= 0){
+            Extension=file.substring(index+1);
+            return Extension;
+        }
+        return "empty";
+    }
+    
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        int length=0;
+        if(txtsonuc.getText().length()>0)
         {
-            for (int i = 0; i < str.length(); i++)
-            {
-                if (str.charAt(i) == ' ')
-                {
-                    count++;
+            String ss;
+            ss=txtsonuc.getText();
+            ss= ss.replaceAll("\\s+", "");
+            length= ss.length();
+            jButton2.setText(Integer.toString(length));
+        }
+        
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jLabel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseClicked
+        System.exit(0);
+    }//GEN-LAST:event_jLabel2MouseClicked
+
+    private void jLabel3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel3MouseClicked
+       this.setState(JFrame.ICONIFIED);
+    }//GEN-LAST:event_jLabel3MouseClicked
+
+    private void PathFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PathFieldActionPerformed
+
+    }//GEN-LAST:event_PathFieldActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+       int length=0;
+        if(txtsonuc.getText().length()>0)
+        {
+            String ss;
+            ss=txtsonuc.getText();
+            ss =ss.trim().replaceAll("\\s+", " ");//trim methodu kullanılarak boşluğu fazladan saymayı engelledi.
+            String[] dize = ss.split(" ");
+            length= dize.length;
+            jButton1.setText(Integer.toString(length));
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
                 }
             }
-            count = count + 1; 
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        return count; // returns 0 if string starts or ends with space " ".
+        //</editor-fold>
+
+ 
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                
+              //  JOptionPane.showMessageDialog(null,"Start Screen!","Welcome",JOptionPane.PLAIN_MESSAGE);
+                new MainScreen().setVisible(true);
+            }
+        });
     }
- }
 
-
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField PathField;
+    private javax.swing.JButton SelectButton;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea txtsonuc;
+    // End of variables declaration//GEN-END:variables
+}
