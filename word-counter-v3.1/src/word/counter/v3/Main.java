02e273f1e6e7a20d7a008483c892a4bde6164adf
RamazanBakir <ramazan9898@hotmail.com>
2019-04-21 01:24:04 +0300

    Version 3.1
    In this version, GUI design was changed.

---
+++ word-counter-v3.1/src/word/counter/v3/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package word.counter.v3;
+
+/**
+ *
+ * @author Ramazan
+ */
+public class Main {
+   public static void main(String[] args)
+    {
+        Wordcounter_v3 mainscreen =new Wordcounter_v3();
+        mainscreen.setVisible(true);
+       
+    } 
+}
+
